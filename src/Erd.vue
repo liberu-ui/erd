9ac35403bf56ad7776e9a3fb87b5f2b81a2ba12b
mishu <mishu@xtelecom.ro>
2021-11-16 11:48:59 +0200

    wip

--- src/Erd.vue
+++ src/Erd.vue
@@ -30,7 +30,7 @@ export default {
     },
 
     render() {
-        return this.$scopedSlots.default({
+        return this.$slots.default({
             width: this.width,
             height: this.height,
         });
